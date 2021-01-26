393ccfd9d583964c47e24c1cecfda30def49c386
tomasnobile <tnobile6@gmail.com>
2021-01-26 15:28:52 -0300

    fix some problems

--- src/modules/alert/alert.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AlertGateway } from './alert.gateway';
-import { AlertController } from './alert.controller';
-
-@Module({
-  controllers: [AlertController],
-  providers: [AlertGateway]
-})
-export class AlertModule {}
