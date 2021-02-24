f16713360ca55c5c999294c0938951506f356d0a
Federico Gomez <fedegomezdev@gmail.com>
2021-02-24 09:18:03 -0300

    change user model

--- src/modules/users/schema/users.schema.ts
+++ src/modules/users/schema/users.schema.ts
@@ -8,40 +8,61 @@ export type UserDocument = User & mongoose.Document;
 export class User {
   @Prop({ trim: true })
   name: string;
+
   @Prop()
   lastName: string;
+  
   @Prop({ required: false })
   provider?: string;
+  
   @Prop({ required: false })
   provider_id: string;
+  
   @Prop({ required: false })
   apple_id?: string;
+  
+  @Prop({type:[String], required: false })
+  photo?: string[];
+  
   @Prop({ required: false })
-  photo?: [string];
+  profilePhoto?: string;
+
   @Prop({ required: false })
   DNI?: number;
+  
   @Prop({ required: false, unique: true })
   email?: string;
+  
   @Prop()
   birthDate: Date;
+  
   @Prop()
   sex: string;
+  
   @Prop({ type: mongoose.SchemaTypes.ObjectId, ref: 'City', autopopulate: true })
   city: City;
+  
   @Prop({ required: false })
   phoneNumber?: number;
+  
   @Prop({ required: false })
   password?: string;
+  
   @Prop({ required: false })
   address?: string;
+  
   @Prop({ required: false })
   passwordRecover?: string;
+  
   @Prop({ default: false, required: false })
   confirmPasswordRecover?: boolean;
+  
   @Prop({ default: false })
   isAdmin: boolean;
+  
   @Prop()
   calification: boolean;
+  
   @Prop()
   comment: string;
 }
