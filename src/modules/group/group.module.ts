19ef17f40a3a5f102beaf4b6229fdf62789924ae
Federico Gomez <fedegomezdev@gmail.com>
2021-02-25 09:33:21 -0300

    add invitations logic, and api global prefix

--- src/modules/group/group.module.ts
+++ src/modules/group/group.module.ts
@@ -7,13 +7,19 @@ import { User, UserSchema } from '../users/schema/users.schema';
 import { InterGroup, InterGroupSchema } from '../inter-group/schema/interGroup.schema';
 import { InterGroupModule } from '../inter-group/interGroup.module';
 import { InterGroupService } from '../inter-group/interGroup.service';
+import { UsersModule } from '../users/users.module';
+import { Invitation, InvitationSchema } from './schema/invitation.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Group.name, schema: GroupSchema }]),
+    MongooseModule.forFeature([
+      { name: Group.name, schema: GroupSchema },
+      { name: Invitation.name, schema: InvitationSchema }
+    ]),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-    MongooseModule.forFeature(([{name: InterGroup.name, schema: InterGroupSchema}])),
-    InterGroupModule
+    MongooseModule.forFeature([{ name: InterGroup.name, schema: InterGroupSchema }]),
+    InterGroupModule,
+    UsersModule
   ],
   controllers: [GroupController],
   providers: [GroupService, InterGroupService]
