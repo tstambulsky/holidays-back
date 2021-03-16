4a624800cbfc5b5a6c12736a21ae0be00b34f43f
unknown <tnobile6@gmail.com>
2021-03-16 10:10:02 -0300

    modify chats

--- src/modules/chat/group-chat/chat.gateway.ts
+++ src/modules/chat/group-chat/chat.gateway.ts
@@ -23,7 +23,7 @@ export class ChatGateway implements OnGatewayConnection {
     const author = await this.chatService.getUserFromSocket(socket);
     const message = await this.chatService.saveMessageGroup(content, author, group);
 
-    this.server.sockets.emit('receive_message_group', message);
+   await this.server.sockets.emit('receive_message_group', message);
   }
 
   @SubscribeMessage('request_all_messages_group')
@@ -33,5 +33,41 @@ export class ChatGateway implements OnGatewayConnection {
     const messages = await this.chatService.getAllMessagesGroup(group, author);
 
     socket.emit('send_all_mesages_group', messages);
+  }
+  
+  @SubscribeMessage('send_message_inter_group')
+  async listenForMessagesInter(@MessageBody() data: MessageDTO, @ConnectedSocket() socket: Socket) {
+    const { content, group } = data;
+    const author = await this.chatService.getUserFromSocket(socket);
+    const message = await this.chatService.saveMessageGroup(content, author, group);
+
+   await this.server.sockets.emit('receive_message_inter_group', message);
+  }
+
+  @SubscribeMessage('request_all_messages_inter_group')
+  async requestAllMessagesInter(@MessageBody() data: MessageDTO, @ConnectedSocket() socket: Socket) {
+    const { interGroup, group } = data;
+    const author = await this.chatService.getUserFromSocket(socket);
+    const messages = await this.chatService.getAllMessagesInterGroup(interGroup, group, author);
+
+    socket.emit('send_all_mesages_inter_group', messages);
+  }
+  
+  @SubscribeMessage('send_message_admin')
+  async listenForMessagesAdmin(@MessageBody() data: MessageDTO, @ConnectedSocket() socket: Socket) {
+    const { content, adminUser, group } = data;
+    const author = await this.chatService.getUserFromSocket(socket);
+    const message = await this.chatService.saveMessageAdmin(content, author, adminUser, group);
+
+   await this.server.sockets.emit('receive_message_admin', message);
+  }
+
+  @SubscribeMessage('request_all_messages_admin')
+  async requestAllMessagesAdmin(@MessageBody() data: MessageDTO, @ConnectedSocket() socket: Socket) {
+    const { group } = data;
+    const author = await this.chatService.getUserFromSocket(socket);
+    const messages = await this.chatService.getAllMessagesAdmin(group, author);
+
+    socket.emit('send_all_mesages_admin', messages);
   }
 }
