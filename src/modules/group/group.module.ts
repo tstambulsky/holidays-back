92f626720d762a7f6c7358643d0729454adf7b31
Federico Gomez <fedegomezdev@gmail.com>
2021-03-25 11:45:36 -0300

    start with notifications

--- src/modules/group/group.module.ts
+++ src/modules/group/group.module.ts
@@ -6,20 +6,21 @@ import { Group, GroupSchema } from './schema/group.schema';
 import { CloudinaryModule } from '../cloudinary/cloudinary.module';
 import { UsersModule } from '../users/users.module';
 import { Invitation, InvitationSchema } from './schema/invitation.schema';
-import { ChatModule } from '../chat/chat.module'
+import { ChatModule } from '../chat/chat.module';
+import { NotificationService } from '../notification/notification.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Group.name, schema: GroupSchema },
-      { name: Invitation.name, schema: InvitationSchema },
+      { name: Invitation.name, schema: InvitationSchema }
     ]),
     forwardRef(() => UsersModule),
     CloudinaryModule,
     forwardRef(() => ChatModule)
   ],
   controllers: [GroupController],
-  providers: [GroupService],
+  providers: [GroupService, NotificationService],
   exports: [GroupService]
 })
 export class GroupModule {}
