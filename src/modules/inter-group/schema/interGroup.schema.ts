47db490de29a513fc8b4dfbe5e98883a5a74b28f
Federico Gomez <fedegomezdev@gmail.com>
2021-03-25 12:37:01 -0300

    start with available validations

--- src/modules/inter-group/schema/interGroup.schema.ts
+++ src/modules/inter-group/schema/interGroup.schema.ts
@@ -10,7 +10,7 @@ export type InterGroupDocument = InterGroup & mongoose.Document;
 export class InterGroup {
   @Prop()
   name: string;
-  
+
   @Prop({ required: true, type: mongoose.SchemaTypes.ObjectId, ref: 'Group' })
   groupOne: Group;
 
@@ -19,6 +19,9 @@ export class InterGroup {
 
   @Prop()
   startDate: Date;
+
+  @Prop({ required: false })
+  endDate: Date;
 
   @Prop()
   startTime?: string;
