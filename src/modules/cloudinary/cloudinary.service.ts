76313517d28c2f0586b5ef6cfc870a1f65156aee
Federico Gomez <fedegomezdev@gmail.com>
2021-03-31 11:01:01 -0300

    solve problem with cloudinary

--- src/modules/cloudinary/cloudinary.service.ts
+++ src/modules/cloudinary/cloudinary.service.ts
@@ -3,19 +3,24 @@ import { Cloudinary } from './cloudinary.provider';
 import { cloudinaryConfig } from '../../config/cloudinary';
 @Injectable()
 export class CloudinaryService {
-  private v2: any
+  private v2: any;
   constructor(
-     @Inject(Cloudinary)
-     private cloudinary
-  ){
-     this.cloudinary.v2.config({
-       cloud_name: cloudinaryConfig.CLOUDINARY_NAME,
-       api_key: cloudinaryConfig.CLOUDINARY_API_KEY,
-       api_secret: cloudinaryConfig.CLOUDINARY_API_SECRET
-     })
-    this.v2 = cloudinary.v2
+    @Inject(Cloudinary)
+    private cloudinary
+  ) {
+    this.cloudinary.v2.config({
+      cloud_name: cloudinaryConfig.CLOUDINARY_NAME,
+      api_key: cloudinaryConfig.CLOUDINARY_API_KEY,
+      api_secret: cloudinaryConfig.CLOUDINARY_API_SECRET
+    });
+    this.v2 = cloudinary.v2;
   }
-  async upload(file: any){
-    return await this.v2.uploader.upload(file)
+  async upload(file: any) {
+    try {
+      const data = await this.v2.uploader.upload(file);
+      return data;
+    } catch (error) {
+      throw new Error(error.message);
+    }
   }
 }
