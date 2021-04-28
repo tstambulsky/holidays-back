1014a308678f4a6237b43390ac088d9484f0d387
unknown <tnobile6@gmail.com>
2021-04-28 10:16:56 -0300

    fix push notifications v 1.6

--- src/modules/notification/notification.service.ts
+++ src/modules/notification/notification.service.ts
@@ -190,6 +190,7 @@ export class NotificationService {
           Emergency_category: 'Emergency'
         }
       };
+      console.log(message);
       await this.sendNotification(token, message);
       const notification = await this.cleanData(token, message);
       await this.createNotification(notification);
