bce0d8a2ccd2ac253d3c50574d83d8eac762c10f
unknown <tnobile6@gmail.com>
2021-04-30 18:25:08 -0300

    try to fix pushs 2.2

--- src/modules/chat/chat.service.ts
+++ src/modules/chat/chat.service.ts
@@ -292,12 +292,12 @@ export class ChatService {
     newMessage.readBy.push(userId)
     await newMessage.save();
      for await (let users of integrants) {
+       if (users != userId) {
           const user = await this.usersService.findOneUser({ _id: users, active: true })
-          if (user.deviceToken && user._id !== userId) {
-            console.log(user._id);
-            console.log(userId);
+          if (user.deviceToken) {
           await this.notificationService.sendNewChatMessage(user.deviceToken, group.name);
         }
+      }
       }
     return newMessage;
   }
